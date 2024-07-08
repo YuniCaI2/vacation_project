dc9857a9eeaa1d32cc031d38ae8b3217d4efb42e
zwc <3540741157@qq.com>
2024-07-08 13:47:41 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 import {Tabbar,TabbarItem,Tab,Tabs,Search,Form, Field, CellGroup, NavBar} from 'vant'
 import 'vant/lib/index.css'
+import { Image as VanImage } from 'vant';
+import { Button } from 'vant';
+
 
 const app = createApp(App)/*创建应用实例 */
 
@@ -18,4 +21,6 @@ app.use(Form);
 app.use(Field);
 app.use(NavBar);
 app.use(CellGroup);
+app.use(VanImage);
+app.use(Button);
 app.mount('#app')
