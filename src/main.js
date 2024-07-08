fc3f3c065e100791b1006cbc57eb636ec80b0569
YuniCaI2 <510927726@qq.com>
2024-07-08 14:52:19 +0800

    互动页面前端框架基本完成

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import {Tabbar,TabbarItem,Tab,Tabs,Search,Form, Field, CellGroup, NavBar,Button,Cell} from 'vant'
+import {Tabbar,TabbarItem,Tab,Tabs,Search,Form, Field, CellGroup, NavBar,Button,Cell,Icon,Uploader} from 'vant'
 import 'vant/lib/index.css'
 import { Image as VanImage } from 'vant';
-import { Button } from 'vant';
 
 
 const app = createApp(App)/*创建应用实例 */
@@ -21,11 +20,8 @@ app.use(Form);
 app.use(Field);
 app.use(NavBar);
 app.use(CellGroup);
-<<<<<<< HEAD
 app.use(Cell)
 app.use(Button)
-=======
-app.use(VanImage);
-app.use(Button);
->>>>>>> ee6f55d8a9b3a64cae7333a9fcb1aacc5f0dacdc
+app.use(Uploader)
+app.use(Icon)
 app.mount('#app')
