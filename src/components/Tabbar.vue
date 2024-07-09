e864ca25008787135ae52ef5c8abc10e45a2954c
zwc <3540741157@qq.com>
2024-07-09 23:50:56 +0800

    Tabbar

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -4,15 +4,46 @@ import { ref } from 'vue';
 export default {
   setup() {
     const active = ref(0);
-    return { active };
+    const icon = {
+      cup1:'./photo-list/奖杯.png',
+      cup2:'./photo-list/奖杯1.png',
+      pen1:'./photo-list/羽毛笔.png',
+      pen2:'./photo-list/羽毛笔1.png',
+      active: 'https://fastly.jsdelivr.net/npm/@vant/assets/user-active.png',
+      inactive:
+        'https://fastly.jsdelivr.net/npm/@vant/assets/user-inactive.png',
+    };
+    return {
+      icon,
+      active,
+    };
   },
 };
 </script>
 
 <template>
+  
 <van-tabbar v-model="active">
-  <van-tabbar-item icon="home-o" to="/reaction">互动</van-tabbar-item>
   <van-tabbar-item icon="notes-o" to="/admiration">赏析</van-tabbar-item>
+  <van-tabbar-item to="/reaction">
+    <span>互动</span>
+    <template #icon="props">
+      <img :src="props.active ? icon.pen2 : icon.pen1" />
+    </template>
+  </van-tabbar-item>
+  
+  <van-tabbar-item >
+    <span>排行榜</span>
+    <template #icon="props">
+      <img :src="props.active ? icon.cup2 : icon.cup1" />
+    </template>
+  </van-tabbar-item>
+  <van-tabbar-item badge="3">
+    <span>好友</span>
+    <template #icon="props">
+      <img :src="props.active ? icon.active : icon.inactive" />
+    </template>
+  </van-tabbar-item>
   <van-tabbar-item icon="contact-o" to="/personal">我的</van-tabbar-item>
 </van-tabbar>
 </template>
